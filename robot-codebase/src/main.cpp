ec2fa18abfe435c08980c0eec3cc413f10651708
brambanik <brambanik@gmail.com>
2025-07-13 18:49:27 -0700

    created freeRTOS outline for robot codebase

--- robot-codebase/src/main.cpp
+++ robot-codebase/src/main.cpp
@@ -1,9 +1,95 @@
 #include <Arduino.h>
+#include "driver/ledc.h"
+#include "driver/adc.h"
 
+// global variables and task handles
+
+TaskHandle_t drive_handle = NULL;
+TaskHandle_t grab_handle = NULL;
+TaskHandle_t reverse_handle = NULL;
+TaskHandle_t raise_basket_handle = NULL;
+TaskHandle_t home_handle = NULL;
 // put function declarations here:
+
+
+
+// create tasks here --> main robot functions
+
+void drive(void* parameters) {
+  //this creates an infinite loop, but it will be interrupted by other actions
+  for(;;) {
+    // driving code here, mostly PID control
+  }
+}
+
+void grab(void* paramters) {
+  // grabbing code here, feel free to change parameters
+}
+
+void reverse(void* parameters) {
+  // reverse code for aligning with zipline
+}
+
+void raise_basket(void* parameters) {
+  // raising basket code here
+}
+
+void home(void* parameters) {
+  // homing sequence, to be run once at startup and then deleted
+}
+
+// add more functions here
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  xTaskCreate(
+    drive, // function to be run
+    "Drive", // description of task
+    1000, // bytes allocated to this stack
+    NULL, // parameters, dependent on function
+    1, // priority
+    &drive_handle // task handle
+  ); 
+
+  xTaskCreate(
+    grab, // function to be run
+    "Grab", // description of task
+    1000, // bytes allocated to this stack
+    NULL, // parameters, dependent on function
+    1, // priority
+    &grab_handle // task handle
+  ); 
+
+  xTaskCreate(
+    reverse, // function to be run
+    "Reverse", // description of task
+    1000, // bytes allocated to this stack
+    NULL, // parameters, dependent on function
+    1, // priority
+    &reverse_handle // task handle
+  ); 
+
+  xTaskCreate(
+    raise_basket, // function to be run
+    "Raise Basket", // description of task
+    1000, // bytes allocated to this stack
+    NULL, // parameters, dependent on function
+    1, // priority
+    &raise_basket_handle // task handle
+  ); 
+
+  xTaskCreate(
+    home, // function to be run
+    "Homing", // description of task
+    1000, // bytes allocated to this stack
+    NULL, // parameters, dependent on function
+    1, // priority
+    &home_handle // task handle
+  ); 
+  
+  
+  
 }
 
 void loop() {
