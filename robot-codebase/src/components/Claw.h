38e23a3c1da9541a2e2ab8620924524750256305
brambanik <brambanik@gmail.com>
2025-07-19 22:28:40 -0700

    Filled out header classes for Claw and DriveMotors

--- robot-codebase/src/components/Claw.h
+++ robot-codebase/src/components/Claw.h
@@ -5,4 +5,30 @@
 #ifndef CLAW_H
 #define CLAW_H
 
+#include <Arduino.h>
+#include "hardware/Motor.h"
+#include "hardware/CustomServo.h"
+
+class Claw
+{
+public:
+    Claw(CustomServo rotationServo, CustomServo grabServo, Motor carriageMotor);
+    void grab();
+    void extend(int distance);
+    void rise(int height);
+    void setTurretAngle(int angle);
+    void rotate(int angle);
+    //blows up the robot
+    void explode();
+    //throws the pet
+    void throwPet();
+
+private:
+    CustomServo rotationServo;
+    CustomServo grabServo;
+    Motor carriageMotor;
+    int currentAngle = 0;
+    int height = 0;
+    int extension = 0;
+};
 #endif //CLAW_H
--- robot-codebase/src/components/DriveMotors.h
+++ robot-codebase/src/components/DriveMotors.h
@@ -5,4 +5,23 @@
 #ifndef DRIVEMOTORS_H
 #define DRIVEMOTORS_H
 
+#include "hardware/CustomServo.h"
+#include "hardware/Motor.h"
+
+class DriveMotors
+{
+public:
+    DriveMotors(Motor leftMotor, Motor rightMotor);
+    void drive(int speed, int direction);
+    void stop();
+    void driveStraight(int speed, int direction);
+    void driveLeftMotor(int speed, int direction);
+    void driveRightMotor(int speed, int direction);
+
+private:
+    int currentDirection;
+    int currentSpeed;
+    Motor leftMotor;
+    Motor rightMotor;
+};
 #endif //DRIVEMOTORS_H
