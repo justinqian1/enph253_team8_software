20eccf97340e54110f1a668d1398f9a9d59c1844
brambanik <brambanik@gmail.com>
2025-07-21 16:55:58 -0700

    Added comments to driveMotors.h

--- robot-codebase/src/components/DriveMotors.cpp
+++ robot-codebase/src/components/DriveMotors.cpp
@@ -1,3 +1,9 @@
 //
 // Created by bram on 16/07/25.
 //
+
+#include "DriveMotors.h"
+#include "hardware/Motor.h"
+
+DriveMotors::DriveMotors(const Motor& lMotor, const Motor& rMotor) : leftMotor(lMotor), rightMotor(rMotor){
+}
--- robot-codebase/src/components/DriveMotors.h
+++ robot-codebase/src/components/DriveMotors.h
@@ -11,16 +11,58 @@
 class DriveMotors
 {
 public:
-    DriveMotors(Motor leftMotor, Motor rightMotor);
-    void drive(int speed, int direction);
+    /**
+     * Creates a DriveMotor object with two motors to drive the robot
+     * @param leftMotor the left motor
+     * @param rightMotor the right motor
+     */
+    DriveMotors(const Motor& leftMotor, const Motor& rightMotor);
+
+    /**
+     * Drives the robot with PID control
+     * @param speed the average speed to drive the robot at
+     */
+    void drivePID(int speed);
+
+    /**
+     * Drives the robot with PID control, with variable parameters
+     * @param speed the average speed to drive the robot at
+     * @param proportional the proportional term for the PID
+     * @param derivative the derivative term for the PID
+     * @param integral the integral term for the PID
+     */
+    void drivePID(int speed, double proportional, double derivative, double integral);
+
+    /**
+     * Stops the robot by stopping both motors
+     */
     void stop();
+
+    /**
+     * Drives the robot without PID control
+     * @param speed the speed at which to drive the robot
+     * @param direction the direction to drive straight
+     */
     void driveStraight(int speed, int direction);
+
+    /**
+     * drives the left motor
+     * @param speed the speed at which to drive the left motor
+     * @param direction the direction to drive the left motor
+     */
     void driveLeftMotor(int speed, int direction);
+
+    /**
+     * drives the right motor
+     * @param speed the speed at which to drive the right motor
+     * @param direction the direction to drive the left motor
+     */
     void driveRightMotor(int speed, int direction);
 
 private:
-    int currentDirection;
-    int currentSpeed;
+    int leftMotorDirection = 1;
+    int rightMotorDirection = 1;
+    int currentSpeed = 0;
     Motor leftMotor;
     Motor rightMotor;
 };
