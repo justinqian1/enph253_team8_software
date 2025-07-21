2b7bb5597af857934e699456c574d44a1fa0dc4d
brambanik <brambanik@gmail.com>
2025-07-21 15:54:01 -0700

    Moved comments to header file

--- robot-codebase/src/hardware/CustomServo.cpp
+++ robot-codebase/src/hardware/CustomServo.cpp
@@ -5,11 +5,6 @@
 #include "CustomServo.h"
 
 
-/**
- * creates a CustomServo object with a default position set to 0
- * @param pin the pin to which this servo is assigned
- * @param channel the PWM channel assigned to this servo
- */
 CustomServo::CustomServo(int pin, int channel) : servoPin(pin), pwmChannel(channel), servoPosition(0), periodHertz(50),
                                                  minPulse(1000), maxPulse(2000)
 {
@@ -17,12 +12,6 @@ CustomServo::CustomServo(int pin, int channel) : servoPin(pin), pwmChannel(chann
     ledcAttachPin(this->servoPin, this->pwmChannel);
 }
 
-/**
- * creates a CustomServo object with a starting position
- * @param pin the pin to which this servo is assigned
- * @param channel the  pwm channel assigned to this servo
- * @param position the initial position of this servo
- */
 CustomServo::CustomServo(int pin, int channel, int position) : servoPin(pin), pwmChannel(channel),
                                                                servoPosition(position), periodHertz(50), minPulse(1000),
                                                                maxPulse(2000)
@@ -31,13 +20,6 @@ CustomServo::CustomServo(int pin, int channel, int position) : servoPin(pin), pw
     ledcAttachPin(this->servoPin, this->pwmChannel);
 }
 
-/**
- * creates a CustomServo object with a variety of parameters
- * @param pin the pin to which this servo is assigned
- * @param channel the PWM channel assigned to this servo
- * @param position the initial position of this servo
- * @param hertz the period used for this servo
- */
 CustomServo::CustomServo(int pin, int channel, int position, int hertz) : servoPin(pin), pwmChannel(channel),
                                                                           servoPosition(position), periodHertz(hertz),
                                                                           minPulse(1000), maxPulse(2000)
@@ -46,15 +28,6 @@ CustomServo::CustomServo(int pin, int channel, int position, int hertz) : servoP
     ledcAttachPin(this->servoPin, this->pwmChannel);
 }
 
-/**
- * creates a CustomServo object with a wider variety of parameters
- * @param pin the pin to which this servo is assigned
- * @param channel the PWM channel assigned to this servo
- * @param position the initial position of this servo
- * @param hertz the period used for this servo
- * @param min the minimum pulse length in microseconds
- * @param max the maximum pulse length in microseconds
- */
 CustomServo::CustomServo(int pin, int channel, int position, int hertz, unsigned long min, unsigned long max) :
     servoPin(pin), pwmChannel(channel), servoPosition(position), periodHertz(hertz), minPulse(min), maxPulse(max)
 {
@@ -62,22 +35,10 @@ CustomServo::CustomServo(int pin, int channel, int position, int hertz, unsigned
     ledcAttachPin(this->servoPin, this->pwmChannel);
 }
 
-/**
- * returns the current position of the servo
- * @return the current position of the servo
- */
 int CustomServo::getPosition() { return this->servoPosition; }
 
-/**
- * returns the pin assignment of this servo
- * @return the pin assigned to this servo
- */
 int CustomServo::getPin() { return this->servoPin; }
 
-/**
- * sets the position of a servo to a specified angle
- * @param position the position (in degrees) to set the servo to
- */
 void CustomServo::setAngle(int position)
 {
     position = constrain(position, 0, 180);
@@ -87,11 +48,6 @@ void CustomServo::setAngle(int position)
     this->servoPosition = position;
 }
 
-/**
- * sets the position of a servo to a specified angle over a time frame (used to slow down servo rotation)
- * @param position the position (in degrees) to set the servo to
- * @param time the time (in milliseconds) to turn the servo
- */
 void CustomServo::setAngle(int position, int time)
 {
     int numTicks = abs(this->servoPosition - position);
@@ -114,31 +70,17 @@ void CustomServo::setAngle(int position, int time)
     }
 }
 
-/**
- * rotates the servo a certain number of degrees, with positive being clockwise
- * @param degrees the number of degrees to rotate
- */
 void CustomServo::rotate(int degrees) {
     setAngle(this->servoPosition +  degrees);
 }
 
 //PRIVATE FUNCTIONS
 
-/**
- * Private function that calculate the length of a pulse required to get to a certain position, in microseconds
- * @param pos the position to calculate the pulse for
- * @return the length of the pulse, in microseconds
- */
 int CustomServo::pulseLength(int pos)
 {
     return map(pos, 0, 180, this->minPulse, this->maxPulse);
 }
 
-/**
- * Private function that calculates the duty cycle required to make the PWM signal produce a certain pulse length
- * @param length the length of the pulse to produce
- * @return a double percentage that represents the % duty cycle
- */
 double CustomServo::dutyCycle(int length)
 {
     double period = 1e6 / static_cast<double>(this->periodHertz);
